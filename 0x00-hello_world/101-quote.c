bb8645628f1fe50ec2f194dfcfa51e86ce84190e
rihab touil <rihab18touil@gmail.com>
2023-04-20 16:19:55 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,7 +2,7 @@
 /**
  * main - 101-qoute.c
  * description - Write a C program that prints exactly
- * and that piece of art is useful - Dora Korpar, 2015-10-19, 
+ * and that piece of art is useful - Dora Korpar, 2015-10-19,
  * followed by a new line, to the standard error.
  * Return: 1
  */
