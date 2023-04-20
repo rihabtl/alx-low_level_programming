6678c8e6dcb73d372c89284230147610d950905a
rihab touil <rihab18touil@gmail.com>
2023-04-20 09:18:42 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include<stdio.h>
-int main ()
+int main(void)
 {
 /* print Programming is like building a multilingual puzzle*/
-puts ("\"Programming is like building a multilingual puzzle"); 
+puts("\"Programming is like building a multilingual puzzle"); 
 /*the program should end with the value 0*/
 return 0;
 }
