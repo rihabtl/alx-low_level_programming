da5ab87562b8fc36306625e0c2f33c4bdd1e0d3a
rihab touil <rihab18touil@gmail.com>
2023-04-25 10:01:11 -0700

    abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _abs - checks the absolute value
+ * @n: is the variable
+ * Return: 0 or absolute value of c
+ */
+
+int _abs(int n)
+{
+if (n >= 0)
+{
+return (n);
+}
+return (-n);
+}
