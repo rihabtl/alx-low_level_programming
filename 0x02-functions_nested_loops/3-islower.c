494082e71176bbaca899fdfcd3241ae2dcaca85c
rihab touil <rihab18touil@gmail.com>
2023-04-25 09:01:43 -0700

    is-lower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * islower - check the code.
+ *
+ * Return: 1 if lowercase if not 0
+ */
+
+int _islower(int c)
+{
+if (c >= 97 && c <= 122)
+{
+return (1);
+}
+return (0);
+}
