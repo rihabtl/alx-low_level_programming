435ca4fc591aa5587e448b64bc5a42bd6364825c
rihab touil <rihab18touil@gmail.com>
2023-04-25 10:56:20 -0700

    24 hours

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -13,12 +13,12 @@ for (h = 0; h < 24; h++)
 {
 for (m = 0; m < 60; m++)
 {
-putchar((h / 10) + '0');
-putchar((h % 10) + '0');
-putchar(':');
-putchar((m / 10) + '0');
-putchar((m % 10) + '0');
-putchar('\n');
+_putchar((h / 10) + '0');
+_putchar((h % 10) + '0');
+_putchar(':');
+_putchar((m / 10) + '0');
+_putchar((m % 10) + '0');
+_putchar('\n');
 }
 }
 }
