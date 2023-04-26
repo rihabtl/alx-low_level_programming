9ec6940003844dd3d0162371e4a8b7b1b138c232
rihab touil <rihab18touil@gmail.com>
2023-04-26 09:45:18 -0700

    test

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -2,15 +2,15 @@
 
 /**
 * main - positive or negative to test function
-*
 * Return: 0
 */
 
 int main(void)
 {
-int i=0;
+	int i;
 
-positive_or_negative(i);
+	i = 0;
+	positive_or_negative(i);
 
 return (0);
 }
