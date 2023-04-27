7aebd9871db7ec74107764ca0a5b39e80ca15e75
rihab touil <rihab18touil@gmail.com>
2023-04-27 06:32:29 -0700

    lines

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * more_numbers - print numbers in lines
+ */
+
+void more_numbers(void)
+{
+int i, j;
+i=0;
+while (i <= 10)
+{
+j=0;
+while (j <= 14)
+{
+if (j >= 10)
+_putchar('1');
+_putchar (j % 10 + '0');
+j++;
+}
+_putchar('\n');
+i++;
+}
+}
