18b8a4fae8a1a5ee79fbee6ff9c3ca6439f9508f
rihab touil <rihab18touil@gmail.com>
2023-04-26 22:54:11 -0700

    uppercase

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _isupper - checks for uppercase characters
+ * Return: 1 is uppercase and 0 if not
+ * @c: the character
+ */
+
+int _isupper(int c)
+{
+
+if (c >= 'A' && c < 'Z')
+
+return (1);
+
+else
+
+return (0);
+}
