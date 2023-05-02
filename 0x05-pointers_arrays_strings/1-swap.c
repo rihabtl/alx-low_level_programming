bdf0781b0bcddf805ab106131d0848eae569dc41
rihab touil <rihab18touil@gmail.com>
2023-05-01 21:35:54 -0700

    swap two integers

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,12 +1,13 @@
 #include "main.h"
 
-/*
- * swap_int - swaps the values of two integer
- * @a: the first integer
- * @b: the second integer
+/**
+ * swap_int - swaps the values of two integers
+ * @a: integer to swap
+ * @b: integer to swap
  */
 
 void swap_int(int *a, int *b)
+
 {
 int c;
 
