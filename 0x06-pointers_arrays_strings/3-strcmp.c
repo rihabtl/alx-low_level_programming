18ffdd811fe41c1894fddd42fbd3151b5da9cf91
rihab touil <rihab18touil@gmail.com>
2023-05-04 08:28:00 -0700

    strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -11,12 +11,14 @@ int _strcmp(char *s1, char *s2)
 {
 int a;
 
-for (a = 0; s1[a] != s2[a] && s2[a] != '\0'; a++)
+a = 0;
+while (s1[a] != s2[a] && s2[a] != '\0')
 {
 if (s1[a] != s2[a])
 {
 return (s1[a] - s2[a]);
 }
+a++;
 }
 return (0);
 }
