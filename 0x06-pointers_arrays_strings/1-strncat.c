bf8016e29eec3b14ef17da473fb14423a2b05b5d
rihab touil <rihab18touil@gmail.com>
2023-05-04 07:31:23 -0700

    strncat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -12,7 +12,12 @@ char *_strncat(char *dest, char *src, int n)
 {
 int a, b;
 
-for (a = 0; dest[a] != '\0'; a++);
+a = 0;
+while (dest[a] != '\0')
+{
+a++;
+}
+
 b = 0;
 while (b < n && src[b] != '\0')
 {
