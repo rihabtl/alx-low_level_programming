2a6dd18f506a5371e34ba6d8a3d35700f1ad88cb
rihab touil <rihab18touil@gmail.com>
2023-05-12 10:30:10 -0700

    mynameis

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include"main.h"
+
+/**
+ * main - prints its name
+ * @argc: counter argument
+ * @argv: array of arguments
+ * Return: 0
+ */
+
+int main(int argc __attribute__((unused)), char *argv[])
+{
+printf("%s\n", argv[0]);
+return (0);
+}
