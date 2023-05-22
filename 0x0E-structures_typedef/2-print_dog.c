6f8b8b7759652f32fb70023e96c7ecec2b10555f
rihab touil <rihab18touil@gmail.com>
2023-05-22 13:20:26 -0700

    hopefully

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,20 @@
+#include "dog.h"
+#include<stdio.h>
+
+/**
+ * print_dog - prints struct dog
+ * @d: dog
+ */
+
+void print_dog(struct dog *d)
+{
+if (d == NULL)
+return;
+if ((*d).name == NULL)
+(*d).name = "(nil)";
+if ((*d).owner == NULL)
+(*d).owner = "(nil)";
+printf("name: %s\n", (*d).name);
+printf("Age: %f\n", (*d).age);
+printf("Owner: %s\n", (*d).owner);
+}
