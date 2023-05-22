8005b3c9ede25bc91e8edf165755c7c81a296aa5
rihab touil <rihab18touil@gmail.com>
2023-05-22 12:20:50 -0700

    woof

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - struct
+ * @name: the name
+ * @age: the age
+ * @owner: the owner
+ * Description: poppy
+ */
+
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+
+#endif
