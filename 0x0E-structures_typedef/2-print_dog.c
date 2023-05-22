4d6184f188b753e0ce8bbe977992fc2b7176e1b7
rihab touil <rihab18touil@gmail.com>
2023-05-22 13:23:53 -0700

    hopefully

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -14,7 +14,7 @@ if ((*d).name == NULL)
 (*d).name = "(nil)";
 if ((*d).owner == NULL)
 (*d).owner = "(nil)";
-printf("name: %s\n", (*d).name);
+printf("Name: %s\n", (*d).name);
 printf("Age: %f\n", (*d).age);
 printf("Owner: %s\n", (*d).owner);
 }
