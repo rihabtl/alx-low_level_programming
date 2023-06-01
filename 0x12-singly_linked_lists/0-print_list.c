569532d5aba86cb5741f29708b14644d50b36d13
rihab touil <rihab18touil@gmail.com>
2023-06-01 11:41:59 -0700

    task 0 again

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,16 +9,19 @@
 
 size_t print_list(const list_t *h)
 {
-	size_t s = 0;
+	size_t nodes = 0;
 
-	while (h != NULL) {
-		if (!(*h).str)
+	while (h)
+	{
+		if (h->str == NULL)
 			printf("[0] (nil)\n");
+
 		else
-			printf("[%u] %s\n", (*h).len, (*h).str);
-		h = (*h).next;
-		s++;
+			printf("[%d] %s\n", h->len, h->str);
+
+		nodes++;
+		h = h->next;
 	}
 
-	return s;
+	return (nodes);
 }
