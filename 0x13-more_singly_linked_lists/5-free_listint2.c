733dac000b44c3ee833153a0a04a5239917425a3
rihab touil <rihab18touil@gmail.com>
2023-06-06 04:54:17 -0700

    5

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -2,10 +2,12 @@
 #include <stdlib.h>
 
 /**
- * free_listint2 - frees a listint_t list
+ * free_listint2 - function that frees a listint_t list
  * @head: a double pointer of lists
- * Return: nothing
+ *
+ * Return: void
  */
+
 void free_listint2(listint_t **head)
 {
 	listint_t *next;
