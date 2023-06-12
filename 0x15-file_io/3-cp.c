e767f182ba75c6ffe6e7d4beb044edc318b3dc4a
rihab touil <rihab18touil@gmail.com>
2023-06-12 16:59:43 -0700

    i dont care about 40 lines anymore but whats wrong with the output

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,70 +1,65 @@
-#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
 
 /**
- * main - Entry point
- * @argc: Input Number of Arguments
- * @argv: Input Arguments
+ * main - copy a file
+ * @argc: how many argument
+ * @argv: arguments
  * Return: 0
  */
+
 int main(int argc, char *argv[])
 {
-	int f_rd, f_wr, rd_bytes, wr_bytes;
+	int f_wr, f_rd;
+	ssize_t bytes_read, bytes_written;
 	char buffer[BUFSIZ];
 
 	if (argc != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		return (97);
+		exit(97);
 	}
 
 	f_rd = open(argv[1], O_RDONLY);
-	if (f_rd == -1)
+	if (f_rd < 0)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		return (98);
+		exit(98);
 	}
 
-	f_wr = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
-	if (f_wr == -1)
+	f_wr = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	if (f_wr < 0)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 		close(f_rd);
-		return (99);
+		exit(99);
 	}
-
-	while ((rd_bytes = read(f_rd, buffer, BUFSIZ)) > 0)
+	while ((bytes_read = read(f_rd, buffer, BUFSIZ)) > 0)
 	{
-		wr_bytes = write(f_wr, buffer, rd_bytes);
-		if (wr_bytes == -1)
+		bytes_written = write(f_wr, buffer, bytes_read);
+		if (bytes_written != bytes_read)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 			close(f_rd);
 			close(f_wr);
-			return (99);
+			exit(99);
 		}
 	}
-
-	if (rd_bytes == -1)
+	if (bytes_read < 0)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		close(f_rd);
-		close(f_wr);
-		return (98);
+		exit(98);
 	}
-
-	if (close(f_rd) == -1)
+	if (close(f_rd) < 0 || close(f_wr) < 0)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", f_rd);
-		return (100);
+		if (close(f_rd) < 0)
+			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", f_rd);
+		if (close(f_wr) < 0)
+			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", f_wr);
+		exit(100);
 	}
-
-	if (close(f_wr) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", f_wr);
-		return (100);
-	}
-
 	return (0);
 }
+
