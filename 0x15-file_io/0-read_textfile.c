9a22fde58bff27f747496ddab0f6e58f9c47157c
rihab touil <rihab18touil@gmail.com>
2023-06-12 12:01:34 -0700

    yes its not correct

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -18,27 +18,13 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
-		return (-1);
-
+		return (0);
 	buf = malloc(sizeof(char) * letters);
-	if (buf == NULL)
-	{
-		close(fd);
-		return (-1);
-	}
-
 	t = read(fd, buf, letters);
-	if (t == -1)
-	{
-		free(buf);
-		close(fd);
-		return (-1);
-	}
-
 	w = write(STDOUT_FILENO, buf, t);
 
 	free(buf);
 	close(fd);
-
 	return (w);
 }
+
