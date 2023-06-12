f51b49fea79d0a29914d378eb8cf7a61cf651a84
rihab touil <rihab18touil@gmail.com>
2023-06-12 16:30:20 -0700

    OMG did i say devide

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,48 +1,41 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <fcntl.h>
-#include <unistd.h>
+
+char *create_buffer(char *file);
+void close_file(int fd);
 
 /**
- * print_error - Prints formatted error message
- * @message: string that contains the error
- * @file: string that represents the name of the file
+ * create_buffer - Allocates 1024 bytes
+ * @file: The name of the file
+ * Return: A pointer to the newly-allocated buffer
  */
-
-void print_error(const char *message, const char *file)
+char *create_buffer(char *file)
 {
-	dprintf(STDERR_FILENO, message, file);
-}
+	char *buffer;
 
-/**
- * open_file - Opens a file with specified flags
- * @file: path of the file to be opened.
- * @flags: flags to be used for opening the file
- * @mode: permission for the new file
- * Return: An integer
- */
+	buffer = malloc(sizeof(char) * 1024);
 
-int open_file(const char *file, int flags, mode_t mode)
-{
-	int fd = open(file, flags, mode);
-
-	if (fd < 0)
+	if (buffer == NULL)
 	{
-		print_error("Error: Can't open file %s\n", file);
-		exit(98);
+		dprintf(STDERR_FILENO,
+				"Error: Can't allocate memory for %s\n", file);
+		exit(99);
 	}
-	return (fd);
+
+	return (buffer);
 }
 
 /**
- * close_file - Closes the file descriptor.
- * @fd: Integer representing the file descriptor to be closed.
- * Return: None.
+ * close_file - Closes file descriptors.
+ * @fd: The file descriptor to be closed.
  */
-
 void close_file(int fd)
 {
-	if (close(fd) < 0)
+	int result;
+
+	result = close(fd);
+
+	if (result == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
 		exit(100);
@@ -50,59 +43,53 @@ void close_file(int fd)
 }
 
 /**
- * copy_file - copies the file
- * @file_from: path of the source file
- * @file_to: path of the destination file
- * Return: 0
+ * main - Copies the contents of a file to another file.
+ * @argc: The number of arguments
+ * @argv: arguments
+ * Return: 0 on success
  */
-
-void copy_file(const char *file_from, const char *file_to)
-{
-	int fd_from, fd_to;
-	char buffer[BUFSIZ];
-	ssize_t bytes_read, bytes_written;
-
-	fd_from = open_file(file_from, O_RDONLY, 0);
-	fd_to = open_file(file_to, O_CREAT | O_WRONLY | O_TRUNC, 0664);
-
-	while ((bytes_read = read(fd_from, buffer, BUFSIZ)) > 0)
-	{
-		bytes_written = write(fd_to, buffer, bytes_read);
-		if (bytes_written != bytes_read)
-		{
-			print_error("Error: Can't write to %s\n", file_to);
-			close_file(fd_from);
-			close_file(fd_to);
-			exit(99);
-		}
-	}
-
-	if (bytes_read < 0)
-	{
-		print_error("Error: Can't read from file %s\n", file_from);
-		exit(98);
-	}
-
-	close_file(fd_from);
-	close_file(fd_to);
-}
-
-/**
- * main - Entry point
- * @argc: Number of Arguments
- * @argv: Arguments
- * Return: 0
- */
-
 int main(int argc, char *argv[])
 {
+	int file_from, file_to, bytes_read, bytes_written;
+	char *buffer;
+
 	if (argc != 3)
 	{
-		print_error("Usage: cp file_from file_to\n", "");
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
 
-	copy_file(argv[1], argv[2]);
+	buffer = create_buffer(argv[2]);
+	file_from = open(argv[1], O_RDONLY);
+	bytes_read = read(file_from, buffer, 1024);
+	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+
+	do {
+		if (file_from == -1 || bytes_read == -1)
+		{
+			dprintf(STDERR_FILENO,
+					"Error: Can't read from file %s\n", argv[1]);
+			free(buffer);
+			exit(98);
+		}
+
+		bytes_written = write(file_to, buffer, bytes_read);
+		if (file_to == -1 || bytes_written == -1)
+		{
+			dprintf(STDERR_FILENO,
+					"Error: Can't write to file %s\n", argv[2]);
+			free(buffer);
+			exit(99);
+		}
+
+		bytes_read = read(file_from, buffer, 1024);
+		file_to = open(argv[2], O_WRONLY | O_APPEND);
+
+	} while (bytes_read > 0);
+
+	free(buffer);
+	close_file(file_from);
+	close_file(file_to);
 
 	return (0);
 }
